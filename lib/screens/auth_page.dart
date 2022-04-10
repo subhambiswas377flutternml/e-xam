77ceaf9737f997cc993308a39419b2b95231adf0
@subhambiswas377flutternml <subhambiswas@gytworkz.com>
2022-04-10 15:22:48 +0530

    created more pages

--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -10,8 +10,31 @@ class AuthPage extends StatelessWidget
   {
   MediaQueryData _info = MediaQuery.of(context);
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Column(
         children: [
+          // Custom AppBar
+          Container(
+            height: _info.size.height*0.14,
+            width: _info.size.width,
+            color: KColors.themeColor,
+            child: Stack(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back_ios),
+                      onPressed: (){Navigator.of(context).pop();},
+                      color: Colors.white,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+
+          SizedBox(height: _info.size.height*0.05,),
+
           // Enrollment No
           Padding(
             padding: EdgeInsets.symmetric(horizontal: _info.size.width*0.03),
@@ -41,6 +64,8 @@ class AuthPage extends StatelessWidget
             ),
           ),
 
+          SizedBox(height: _info.size.height*0.05,),
+
           // Password
           Padding(
             padding: EdgeInsets.symmetric(horizontal: _info.size.width*0.03),
@@ -69,6 +94,28 @@ class AuthPage extends StatelessWidget
               ),
             ),
           ),
+          const Spacer(),
+          // Sign up /  Login Button
+          SizedBox(
+            height: _info.size.height*0.07,
+            width: _info.size.width*0.5,
+            child: ElevatedButton(
+              child: const Text("Lets Go"),
+              onPressed: (){
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                  primary: KColors.themeColor,
+                  textStyle: TextStyle(
+                    fontSize: _info.size.width*0.05,
+                    fontWeight: FontWeight.w500,
+                  )
+              ),
+            ),
+          ),
+          SizedBox(height: _info.size.height*0.1,),
         ],
       ),
     );
